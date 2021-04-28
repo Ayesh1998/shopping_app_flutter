cffe70d83fa870f4bfed471dcde4d58c686e4406
Ayesh1998 <ayeshlak1998@gmail.com>
2021-04-28 10:39:46 +0530

    Added Provider package and implemented product providers and used them in app to handle products and favourite states.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/providers/products_provider.dart';
 import 'package:shopping_app/screens/product_details.dart';
 import 'package:shopping_app/screens/products_overview_screen.dart';
 
@@ -7,15 +9,22 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (BuildContext context) {
+        return ProductsProvider();
+      },
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen()
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen()},
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,36 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/providers/product_model.dart';
 import 'package:shopping_app/screens/product_details.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem(this.id, this.imageUrl, this.title);
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+  //
+  // ProductItem(this.id, this.imageUrl, this.title);
 
   @override
   Widget build(BuildContext context) {
+    final selectedProduct = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context)
-                .pushNamed(ProductDetailsScreen.routeName, arguments: id);
+            Navigator.of(context).pushNamed(ProductDetailsScreen.routeName,
+                arguments: selectedProduct.id);
           },
           child: Image.network(
-            imageUrl,
+            selectedProduct.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+            icon: Icon(
+              selectedProduct.isFavourite
+                  ? Icons.favorite
+                  : Icons.favorite_border,
+            ),
+            onPressed: () {
+              selectedProduct.toggleFavourite();
+            },
             color: Theme.of(context).accentColor,
           ),
           title: Text(
-            title,
+            selectedProduct.title,
             textAlign: TextAlign.center,
           ),
           backgroundColor: Colors.black54,
